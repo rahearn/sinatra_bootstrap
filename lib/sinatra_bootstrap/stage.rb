fdcbe77ea2d0fdd9cffd11131f3add83c99a5894
Justin MacAir <jakrup@puppypunchpro.com>
2012-11-17 19:36:12 -0500

    added --pow option to create a tmp/always_restart.txt

--- lib/sinatra_bootstrap/stage.rb
+++ lib/sinatra_bootstrap/stage.rb
@@ -9,10 +9,13 @@ module SinatraBootstrap
     end
 
     desc 'sinatra', 'Installs Gemfile and main.rb'
+    method_option :pow, :type => :boolean, :desc => 'Create a tmp/always_restart.txt for pow', :default => false
     method_option :rackup, :type => :boolean, :desc => 'Create a config.ru file', :default => false
     def sinatra
       copy_file 'Gemfile-bare', 'Gemfile'
       copy_file 'config.ru' if options[:rackup]
+      Dir::mkdir("tmp") unless File.exists?("tmp") if options[:pow]
+      File.open("tmp/always_restart.txt", "w") {} if options[:pow]
       copy_file 'main.rb'
       say 'Simple startup:'
       say '  bundle install'
